d06d6f21ba4e559d9bbf3cd9fa6ac9edd08cac1d
Hoang Ba Khang <hoangbakhang29042005dalat@gmail.com>
2026-05-28 21:35:34 +0700

    Update the UI of the dropdown box in Clone Column function

--- lab-1-fu-grading-with-flutter/DeserializeFGFile/DeserializeFGFile/obj/Debug/net8.0/DeserializeFGFile.AssemblyInfo.cs
+++ lab-1-fu-grading-with-flutter/DeserializeFGFile/DeserializeFGFile/obj/Debug/net8.0/DeserializeFGFile.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeserializeFGFile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82e32e666e9920ae43e8ed067c299912c8f2d022")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41bf4033a767a922e17b7d6f8df645b259c0321a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeserializeFGFile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeserializeFGFile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
